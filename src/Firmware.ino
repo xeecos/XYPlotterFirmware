fb8500086604cbef81830984f8b32f46c5c6ca21
xeecos <flashindream@gmail.com>
2017-11-09 13:49:38 +0800

    update

--- src/Firmware.ino
+++ src/Firmware.ino
@@ -4,7 +4,7 @@
 #define CBI(n, b) (n &= ~_BV(b))
 
 #define POINTS_COUNT 6
-#define PULSES_PER_MM 100
+#define PULSES_PER_MM 10
 #define CURVE_SECTION 0.5
 #define NUM_STEPS 20
 #define NUM_AXIS 2
@@ -15,6 +15,16 @@ enum MOTION_STATE
     MOTION_NORMAL = 1,
     MOTION_DECELETING = 2
 };
+typedef struct
+{
+    volatile double x;
+    volatile double y;
+} Point;
+typedef struct
+{
+    Point start;
+    Point end;
+} Points;
 typedef struct
 {
     volatile long err;
@@ -52,6 +62,7 @@ typedef struct
     volatile uint8_t plannedLength = 0;
     volatile uint8_t plannedIndex = 0;
     volatile uint8_t isRunningState = MOTION_ACCELETING;
+    volatile bool isRelative = false;
     uint8_t powerPin = 10;
     String buffer = "";
 } SystemStatus;
@@ -91,7 +102,7 @@ void parseBuffer(char c)
 {
     if (c == '\n')
     {
-        parseCommand(_sys.buffer);
+        parseCommand();
         _sys.buffer = "";
     }
     else
@@ -100,133 +111,100 @@ void parseBuffer(char c)
     }
 }
 
-static String vStr = "";
-static double v[8];
-void parseCommand(String cmd)
+void parseCommand()
 {
-    cmd.toLowerCase();
-    char code = cmd.charAt(0);
-    long x1, y1, x2, y2, x3, y3, x4, y4;
-    uint8_t vIndex = 0;
-    uint8_t len = cmd.length();
-    vStr = "";
-    for (int i = 1; i < len; i++)
+    _sys.buffer.toLowerCase();
+    int cmdId;
+    if (_sys.buffer.length() > 2)
     {
-        char c = cmd.charAt(i);
-        if (c == ' ' || c == ',')
+        if (hasCommand('g'))
         {
-            if (vIndex > 0)
+            cmdId = getCommand('g');
+            switch (cmdId)
             {
-                v[vIndex - 1] = vStr.toDouble();
+            case 0:
+            case 1:
+            {
+                if (hasCommand('p'))
+                {
+                    firePower(getCommand('p'));
+                }
+                if (hasCommand('f'))
+                {
+                    setSpeed(getCommand('f'));
+                }
+                long x = (hasCommand('x') ? getCommand('x') : 0) * PULSES_PER_MM;
+                long y = (hasCommand('y') ? getCommand('y') : 0) * PULSES_PER_MM;
+                if (_sys.isRelative)
+                {
+                    x += _sys.targetPosition[0];
+                    y += _sys.targetPosition[1];
+                }
+                lineTo(x, y);
+                _sys.targetPosition[0] = x;
+                _sys.targetPosition[1] = y;
             }
-            vStr = "";
-            vIndex++;
-        }
-        else
-        {
-            vStr += c;
-            if (vIndex > 0 && i == len - 1)
+            break;
+            case 28:
             {
-                v[vIndex - 1] = vStr.toDouble();
+                waitingForFinish();
+                _sys.currentPosition[0] = 0;
+                _sys.currentPosition[1] = 0;
             }
-        }
-    }
-    switch (code)
-    {
-    case 'm':
-    {
-        x1 = v[0] * PULSES_PER_MM;
-        y1 = v[1] * PULSES_PER_MM;
-        moveTo(x1, y1);
-        _sys.lastPosition[0] = x1;
-        _sys.lastPosition[1] = y1;
-        break;
-    }
-    case 'l':
-    {
-        x1 = v[0] * PULSES_PER_MM;
-        y1 = v[1] * PULSES_PER_MM;
-        lineTo(x1, y1);
-        _sys.lastPosition[0] = x1;
-        _sys.lastPosition[1] = y1;
-        break;
-    }
-    case 'h':
-    {
-        x1 = v[0] * PULSES_PER_MM;
-        lineTo(x1, _sys.lastPosition[1]);
-        _sys.lastPosition[0] = x1;
-        break;
-    }
-    case 'v':
-    {
-        y1 = v[0] * PULSES_PER_MM;
-        lineTo(_sys.lastPosition[0], y1);
-        _sys.lastPosition[1] = y1;
-        break;
-    }
-    case 'c':
-    {
-        x1 = v[0] * PULSES_PER_MM;
-        y1 = v[1] * PULSES_PER_MM;
-        x2 = v[2] * PULSES_PER_MM;
-        y2 = v[3] * PULSES_PER_MM;
-        x3 = v[4] * PULSES_PER_MM;
-        y3 = v[5] * PULSES_PER_MM;
-        curveTo(_sys.lastPosition[0], _sys.lastPosition[1], x1, y1, x2, y2, x3, y3);
-        _sys.lastPosition[0] = x3;
-        _sys.lastPosition[1] = y3;
-        break;
-    }
-    case 'q':
-    {
-        x1 = v[0] * PULSES_PER_MM;
-        y1 = v[1] * PULSES_PER_MM;
-        x2 = v[2] * PULSES_PER_MM;
-        y2 = v[3] * PULSES_PER_MM;
-        quadTo(_sys.lastPosition[0], _sys.lastPosition[1], x1, y1, x2, y2);
-        _sys.lastPosition[0] = x2;
-        _sys.lastPosition[1] = y2;
-        break;
-    }
-    case 'z':
-    {
-        lineTo(_sys.startPosition[0], _sys.startPosition[1]);
-        _sys.lastPosition[0] = _sys.startPosition[0];
-        _sys.lastPosition[1] = _sys.startPosition[1];
-        break;
-    }
-    case 'p':
-    {
-        setPower(v[0]);
-        setSpeed(v[1]);
-        setAccel(v[2]);
-        break;
-    }
-    case 'e':
-    {
-        firePower(v[0]);
-        break;
-    }
-    case 'o':
-    {
-        waitingForFinish();
-        if (v[0] > 0)
-        {
-            for (int i = 0; i < NUM_AXIS; i++)
+            break;
+            case 90:
             {
-                steppers[i].enableOutputs();
+                waitingForFinish();
+                _sys.isRelative = false;
+            }
+            break;
+            case 91:
+            {
+                waitingForFinish();
+                _sys.isRelative = true;
+            }
+            break;
             }
         }
-        else
+        else if (hasCommand('m'))
         {
-            for (int i = 0; i < NUM_AXIS; i++)
+            cmdId = getCommand('m');
+            switch (cmdId)
             {
-                steppers[i].disableOutputs();
+            case 1:
+            {
+                waitingForFinish();
+                if (hasCommand('p') && getCommand('p') == 1)
+                {
+                    for (int i = 0; i < NUM_AXIS; i++)
+                    {
+                        steppers[i].enableOutputs();
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < NUM_AXIS; i++)
+                    {
+                        steppers[i].disableOutputs();
+                    }
+                }
+                break;
+            }
+            case 4:
+            {
+                waitingForFinish();
+                if (hasCommand('p'))
+                {
+                    firePower(getCommand('p'));
+                }
+                else
+                {
+                    firePower(0);
+                }
+                break;
+            }
             }
         }
-        break;
-    }
     }
     finishCommand();
 }
@@ -296,10 +274,6 @@ void closePower()
 {
     firePower(0);
 }
-/**
- * motion control in Timer
- */
-
 void shiftPlanned()
 {
     if (_sys.plannedLength > 0)
@@ -315,12 +289,10 @@ void shiftPlanned()
 void lockTimer()
 {
     cli();
-    // noInterrupts();
 }
 void unlockTimer()
 {
     sei();
-    // interrupts();
 }
 void setRun()
 {
@@ -512,8 +484,7 @@ void moveTo(long x, long y)
 #endif
     _sys.startPosition[0] = x;
     _sys.startPosition[1] = y;
-    _sys.currentSpeed = _sys.defaultSpeed;
-    addMotion(x, y, 0, _sys.speed, _sys.acceleration);
+    addMotion(x, y, 0, _sys.maxSpeed, _sys.acceleration);
 }
 void lineTo(long x, long y)
 {
@@ -529,116 +500,41 @@ void lineTo(long x, long y)
 #endif
     addMotion(x, y, _sys.power, _sys.speed, _sys.acceleration);
 }
-void curveTo(long x0, long y0, long x1, long y1, long x2, long y2, long x3, long y3)
-{
-    double subdiv_step = 1.0 / (NUM_STEPS + 1);
-    double subdiv_step2 = subdiv_step * subdiv_step;
-    double subdiv_step3 = subdiv_step * subdiv_step * subdiv_step;
-    double pre1 = 3.0 * subdiv_step;
-    double pre2 = 3.0 * subdiv_step2;
-    double pre4 = 6.0 * subdiv_step2;
-    double pre5 = 6.0 * subdiv_step3;
-    double tmp1x = x0 - x1 * 2.0 + x2;
-    double tmp1y = y0 - y1 * 2.0 + y2;
-    double tmp2x = (x1 - x2) * 3.0 - x0 + x3;
-    double tmp2y = (y1 - y2) * 3.0 - y0 + y3;
-    double fx = x0;
-    double fy = y0;
-    double dfx = (x1 - x0) * pre1 + tmp1x * pre2 + tmp2x * subdiv_step3;
-    double dfy = (y1 - y0) * pre1 + tmp1y * pre2 + tmp2y * subdiv_step3;
-    double ddfx = tmp1x * pre4 + tmp2x * pre5;
-    double ddfy = tmp1y * pre4 + tmp2y * pre5;
-    double dddfx = tmp2x * pre5;
-    double dddfy = tmp2y * pre5;
-    int step = NUM_STEPS;
-#ifdef DEBUG
-    String str = "curve:";
-    str += x0;
-    str += ",";
-    str += y0;
-    str += " ";
-    str += x1;
-    str += ",";
-    str += y1;
-    str += " ";
-    str += x2;
-    str += ",";
-    str += y2;
-    str += " ";
-    str += x3;
-    str += ",";
-    str += y3;
-    Serial.println(str);
-#endif
-    while (step--)
-    {
-        fx += dfx;
-        fy += dfy;
-        dfx += ddfx;
-        dfy += ddfy;
-        ddfx += dddfx;
-        ddfy += dddfy;
-        lineTo(floorf(fx * 100) / 100, floorf(fy * 100) / 100);
-    }
-    lineTo(floorf(x3 * 100) / 100, floorf(y3 * 100) / 100);
-}
-void quadTo(long x0, long y0, long x1, long y1, long x2, long y2)
-{
-#ifdef DEBUG
-    String str = "quad:";
-    str += " ";
-    str += x1;
-    str += ",";
-    str += y1;
-    str += " ";
-    str += x2;
-    str += ",";
-    str += y2;
-    Serial.println(str);
-#endif
-    int step = NUM_STEPS;
-    double fx, fy;
-    for (int i = 0; i < step; i++)
-    {
-        float t = (float)i / (float)step;
-        float u = 1.0 - t;
-        fx = x0 * u * u + x1 * 2 * u * t + x2 * t * t;
-        fy = y0 * u * u + y1 * 2 * u * t + y2 * t * t;
-        lineTo(floorf(fx * 100) / 100, floorf(fy * 100) / 100);
-    }
-    lineTo(floorf(x2 * 100) / 100, floorf(y2 * 100) / 100);
-}
 
-void arc(long sx, long sy, long cx, long cy, long ex, long ey, bool ccw)
+bool hasCommand(char cmd)
 {
-    double angleA, angleB, angle, radius, length, aX, aY, bX, bY;
-    aX = (sx - cx);
-    aY = (sy - cy);
-    bX = (ex - cx);
-    bY = (ey - cy);
-    if (ccw)
+    int index = 0;
+    char c = 0;
+    while (c != cmd)
     {
-        angleA = atan2(aY, aX);
-        angleB = atan2(bY, bX);
+        if (index >= _sys.buffer.length())
+        {
+            return false;
+        }
+        else
+        {
+            c = _sys.buffer.charAt(index);
+        }
+        index++;
     }
-    else
+    return true;
+}
+double getCommand(char cmd)
+{
+    int index = _sys.buffer.indexOf(cmd);
+    int startIndex = _sys.buffer.indexOf(cmd) + 1;
+    char c = 0;
+    while (c != ' ')
     {
-        angleA = atan2(bY, bX);
-        angleB = atan2(aY, aX);
+        index++;
+        if (index >= _sys.buffer.length())
+        {
+            break;
+        }
+        else
+        {
+            c = _sys.buffer.charAt(index);
+        }
     }
-    if (angleB <= angleA)
-    {
-        angleB += 2 * M_PI;
-    }
-    angle = angleB - angleA;
-    radius = sqrt(aX * aX + aY * aY);
-    length = radius * angle;
-    int steps, s, step;
-    steps = (int)ceil(length / CURVE_SECTION);
-    for (s = 1; s <= steps; s++)
-    {
-        step = (ccw) ? s : (steps - s);
-        float t = step / steps;
-        lineTo(cx + radius * cos(angleA + angle * t), cy + radius * sin(angleA + angle * t));
-    }
+    return _sys.buffer.substring(startIndex, index).toDouble();
 }
