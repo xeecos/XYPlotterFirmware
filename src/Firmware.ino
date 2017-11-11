82d5f9ff5c139e40ecc91987838f64fd00e88e99
xeecos <flashindream@gmail.com>
2017-11-11 18:42:20 +0800

    update

--- src/Firmware.ino
+++ src/Firmware.ino
@@ -2,8 +2,7 @@
 #include <MeStepper.h>
 #define SBI(n, b) (n |= _BV(b))
 #define CBI(n, b) (n &= ~_BV(b))
-
-#define POINTS_COUNT 8
+#define POINTS_COUNT 100
 #define PULSES_PER_MM 100
 #define CURVE_SECTION 0.5
 #define NUM_STEPS 10
@@ -26,22 +25,21 @@ typedef struct
     volatile long targetCount[NUM_AXIS];
     volatile long totalCount = 0;
     volatile uint16_t speed = 1000;
-    volatile uint16_t exitSpeed = 100;
+    volatile uint16_t exitSpeed = 1000;
     volatile uint16_t acceleration = 1;
     volatile uint8_t power = 0;
 } PlannerPoint;
 typedef struct
 {
-    bool running = false;
     volatile long currentPosition[NUM_AXIS] = {0, 0};
     volatile long targetPosition[NUM_AXIS];
     volatile long startPosition[NUM_AXIS];
     volatile long lastPosition[NUM_AXIS] = {0, 0};
-    long defaultAcceleration = 64;
-    long defaultAccelCount = 200;
-    long defaultDecelCount = 200;
-    uint16_t defaultSpeed = 1000;
-    volatile long currentSpeed = 1;
+    volatile long defaultAcceleration = 64;
+    volatile long defaultAccelCount = 200;
+    volatile long defaultDecelCount = 200;
+    volatile uint16_t defaultSpeed = 1000;
+    volatile long currentSpeed = 100;
     volatile uint8_t power = 0;
     volatile uint16_t speed = 1000;
     volatile uint16_t acceleration = 100;
@@ -50,19 +48,23 @@ typedef struct
     volatile uint8_t isRunningState = MOTION_ACCELETING;
     volatile bool isRelative = false;
     uint8_t powerPin = 10;
-    String buffer = "";
+    String buffer;
+    volatile int bufferIndex = 0;
+    volatile long during = 1000;
+    volatile long currentTime = 0;
 } SystemStatus;
 static SystemStatus _sys;
 static PlannerPoint _points[POINTS_COUNT];
 static PlannerPoint _prevPoint;
 volatile bool busy = false;
+volatile bool isbusy = false;
 MeStepper steppers[NUM_AXIS] = {MeStepper(SLOT_1), MeStepper(SLOT_2)};
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(250000);
     delay(1000);
-    initTimer();
+    // initTimer();
     for (int i = 0; i < NUM_AXIS; i++)
     {
         steppers[i].setMicroStep(16);
@@ -71,61 +73,103 @@ void setup()
     Serial.println("opened");
     setPower(0);
     moveTo(0, 0);
-    for (;;)
-    {
-        if (Serial.available())
-        {
-            char c = Serial.read();
-            parseBuffer(c);
-        }
-    }
+    finishCommand();
 }
 
 void loop()
 {
-}
-void parseBuffer(char c)
-{
-    if (c == '\n')
+    if (Serial.available())
     {
-        parseCommand();
-        _sys.buffer = "";
-    }
-    else
-    {
-        _sys.buffer += c;
-    }
-}
-
-void parseCommand()
-{
-    _sys.buffer.toLowerCase();
-    int cmdId;
-    if (_sys.buffer.length() > 2)
-    {
-        if (hasCommand('g'))
+        char c = Serial.read();
+        if (c == '\n')
         {
-            cmdId = getCommand('g');
+            finishCommand();
+            // if (!isbusy)
+            parseCommand(_sys.buffer);
+            _sys.buffer = "";
+            // _sys.bufferIndex = 0;
+            // for (int i = 0; i < 128; i++)
+            // {
+            //     _sys.buffer[i] = 0;
+            // }
+        }
+        else
+        {
+            _sys.buffer += c;
+            // _sys.buffer[_sys.bufferIndex] = c;
+            // _sys.bufferIndex++;
+            // if (_sys.bufferIndex > 128)
+            // {
+            //     _sys.bufferIndex = 0;
+            //     while (Serial.read() != '\n')
+            //     {
+            //         break;
+            //     }
+            //     Serial.println("error!!!!!");
+            // }
+        }
+    }
+    motion();
+}
+void parseCommands()
+{
+    isbusy = true;
+    // cli();
+    // String cmds;
+    // int i = 0;
+    // for (i = 0; i <= _sys.bufferIndex; i++)
+    // {
+    //     cmds += _sys.buffer[i];
+    // }
+    // int counter = 0;
+    // int lastIndex = 0;
+    // for (i = 0; i < cmds.length(); i++)
+    // {
+    //     if (cmds.charAt(i) == ';')
+    //     {
+    //         parseCommand(cmds.substring(lastIndex, i));
+    //         finishCommand();
+    //         lastIndex = i + 1;
+    //     }
+    // }
+    // parseCommand(cmds.substring(lastIndex, i));
+    // sei();
+    isbusy = false;
+    // if (_sys.plannedLength % 2 == 0)
+}
+void parseCommand(String cmd)
+{
+    cmd.toLowerCase();
+    // Serial.println(cmd);
+    int cmdId;
+    if (cmd.length() > 2)
+    {
+        if (hasCommand(cmd, 'g'))
+        {
+            cmdId = getCommand(cmd, 'g');
             switch (cmdId)
             {
             case 0:
             case 1:
             {
-                if (hasCommand('p') && cmdId == 1)
+                if (hasCommand(cmd, 'p') && cmdId == 1)
                 {
-                    setPower(getCommand('p'));
+                    setPower(getCommand(cmd, 'p'));
                 }
                 if (cmdId == 0)
                 {
                     setPower(0);
                 }
-                if (hasCommand('f'))
+                if (hasCommand(cmd, 'f'))
                 {
-                    setSpeed(getCommand('f'));
-                    setAccel(200);
+                    setSpeed(getCommand(cmd, 'f'));
                 }
-                double x = (hasCommand('x') ? getCommand('x') : 0) * PULSES_PER_MM;
-                double y = (hasCommand('y') ? getCommand('y') : 0) * PULSES_PER_MM;
+                if (hasCommand(cmd, 'a'))
+                {
+                    setAccel(getCommand(cmd, 'a'));
+                }
+                double x = (hasCommand(cmd, 'x') ? fmax(-30, fmin(30, getCommand(cmd, 'x'))) * PULSES_PER_MM : _sys.targetPosition[0]);
+                double y = (hasCommand(cmd, 'y') ? fmax(-30, fmin(30, getCommand(cmd, 'y'))) * PULSES_PER_MM : _sys.targetPosition[1]);
                 if (_sys.isRelative)
                 {
                     x += _sys.targetPosition[0];
@@ -164,15 +208,15 @@ void parseCommand()
             break;
             }
         }
-        else if (hasCommand('m'))
+        else if (hasCommand(cmd, 'm'))
         {
-            cmdId = getCommand('m');
+            cmdId = getCommand(cmd, 'm');
             switch (cmdId)
             {
             case 1:
             {
                 waitingForFinish();
-                if (hasCommand('p') && getCommand('p') == 1)
+                if (hasCommand(cmd, 'p') && getCommand(cmd, 'p') == 1)
                 {
                     for (int i = 0; i < NUM_AXIS; i++)
                     {
@@ -191,9 +235,9 @@ void parseCommand()
             case 4:
             {
                 waitingForFinish();
-                if (hasCommand('p'))
+                if (hasCommand(cmd, 'p'))
                 {
-                    firePower(getCommand('p'));
+                    firePower(getCommand(cmd, 'p'));
                 }
                 else
                 {
@@ -204,7 +248,6 @@ void parseCommand()
             }
         }
     }
-    finishCommand();
 }
 
 void addMotion(long x, long y, uint8_t power, uint16_t speed, uint16_t acceleration)
@@ -219,14 +262,14 @@ void addMotion(long x, long y, uint8_t power, uint16_t speed, uint16_t accelerat
     _points[_sys.plannedIndex].delta[1] = (_points[_sys.plannedIndex].targetCount[1]);
     _points[_sys.plannedIndex].totalCount = _points[_sys.plannedIndex].delta[0] + _points[_sys.plannedIndex].delta[1];
     _points[_sys.plannedIndex].speed = speed;
-    if (_sys.plannedIndex > 1)
+    if (_sys.plannedIndex > 0)
     {
         _points[_sys.plannedIndex - 1].exitSpeed = _points[_sys.plannedIndex].speed;
     }
     _points[_sys.plannedIndex].acceleration = acceleration;
     long dist = sqrt(_points[_sys.plannedIndex].delta[0] * _points[_sys.plannedIndex].delta[0] + _points[_sys.plannedIndex].delta[1] * _points[_sys.plannedIndex].delta[1]) / 2;
     _points[_sys.plannedIndex].accelCount = _points[_sys.plannedIndex].totalCount - min(dist, _sys.defaultAccelCount);
-    _points[_sys.plannedIndex].decelCount = min(dist, _sys.defaultDecelCount);
+    _points[_sys.plannedIndex].decelCount = 3; //min(dist, _sys.defaultDecelCount);
     _points[_sys.plannedIndex].power = power;
     _points[_sys.plannedIndex].err = (_points[_sys.plannedIndex].delta[0] > _points[_sys.plannedIndex].delta[1] ? _points[_sys.plannedIndex].delta[0] : -_points[_sys.plannedIndex].delta[1]) / 2;
 
@@ -284,22 +327,6 @@ void shiftPlanned()
         _sys.plannedLength -= 1;
     }
 }
-void lockTimer()
-{
-    cli();
-}
-void unlockTimer()
-{
-    sei();
-}
-void setRun()
-{
-    _sys.running = true;
-}
-void setStop()
-{
-    _sys.running = false;
-}
 uint8_t isRunningState()
 {
     return _sys.isRunningState;
@@ -346,24 +373,27 @@ void initTimer()
     TCCR1A = 0;
     TCCR1B = 0;
     TCNT1 = 0;
-    OCR1A = 2000;
+    OCR1A = 100;
     TCCR1B |= (1 << WGM12);
     TCCR1B |= (1 << CS11);
     TIMSK1 |= (1 << OCIE1A);
     sei();
 }
 int tt = 0;
-ISR(TIMER1_COMPA_vect)
+// ISR(TIMER1_COMPA_vect)
+void motion()
 {
-    if (busy)
+    if (micros() - _sys.currentTime >= _sys.during)
     {
-        return;
+        // if (busy)
+        // {
+        //     return;
+        // }
+        // busy = true;
+        run();
+        // busy = false;
+        _sys.currentTime = micros();
     }
-    sei();
-    busy = true;
-    run();
-    busy = false;
-    cli();
 }
 void run()
 {
@@ -378,7 +408,7 @@ bool motionStep()
     {
         return false;
     }
-    firePower(_points[0].power);
+    // firePower(_points[0].power);
     long distX = _points[0].delta[0];
     long distY = _points[0].delta[1];
     if (distX <= 0 && distY <= 0)
@@ -413,56 +443,53 @@ void stepY(bool dir)
 }
 void nextWait()
 {
-    if (tt % 2 == 0)
+    if (_points[0].totalCount > _points[0].accelCount)
     {
-        if (_points[0].totalCount > _points[0].accelCount)
-        {
-            _sys.isRunningState = MOTION_ACCELETING;
-        }
-        else if (_points[0].totalCount > _points[0].decelCount)
-        {
-            _sys.isRunningState = MOTION_NORMAL;
-        }
-        else
-        {
-            _sys.isRunningState = MOTION_DECELETING;
-        }
-        switch (_sys.isRunningState)
-        {
-        case MOTION_ACCELETING:
-        {
-            if (_sys.currentSpeed < _points[0].speed)
-            {
-                _sys.currentSpeed += _points[0].acceleration;
-                if (_sys.currentSpeed > _points[0].speed)
-                {
-                    _sys.currentSpeed = _points[0].speed;
-                }
-            }
-            break;
-        }
-        case MOTION_DECELETING:
-        {
-            if (_sys.currentSpeed > _points[0].exitSpeed)
-            {
-                _sys.currentSpeed -= _points[0].acceleration;
-                if (_sys.currentSpeed < _points[0].exitSpeed)
-                {
-                    _sys.currentSpeed = _points[0].exitSpeed;
-                }
-            }
-            break;
-        }
-        case MOTION_NORMAL:
-        {
-            // OCR1A;
-            break;
-        }
-        }
-        tt = 0;
-        OCR1A = 1600000 / _sys.currentSpeed - 1;
+        _sys.isRunningState = MOTION_ACCELETING;
     }
-    tt++;
+    else if (_points[0].totalCount > _points[0].decelCount)
+    {
+        _sys.isRunningState = MOTION_NORMAL;
+    }
+    else
+    {
+        _sys.isRunningState = MOTION_DECELETING;
+    }
+    switch (_sys.isRunningState)
+    {
+    case MOTION_ACCELETING:
+    {
+        if (_sys.currentSpeed < _points[0].speed)
+        {
+            _sys.currentSpeed += _points[0].acceleration;
+            if (_sys.currentSpeed > _points[0].speed)
+            {
+                _sys.currentSpeed = _points[0].speed;
+            }
+        }
+        break;
+    }
+    case MOTION_DECELETING:
+    {
+        if (_sys.currentSpeed > _points[0].exitSpeed)
+        {
+            _sys.currentSpeed -= _points[0].acceleration;
+            if (_sys.currentSpeed < _points[0].exitSpeed)
+            {
+                _sys.currentSpeed = _points[0].exitSpeed;
+            }
+        }
+        break;
+    }
+    case MOTION_NORMAL:
+    {
+        // OCR1A;
+        break;
+    }
+    }
+    tt = 0;
+    _sys.during = 1600000 / _sys.currentSpeed - 1;
+    _sys.during = _sys.during > 10000 ? 10000 : (_sys.during < 10 ? 10 : _sys.during);
 }
 /**
  * Motion Parse
@@ -497,40 +524,40 @@ void lineTo(long x, long y)
     addMotion(x, y, _sys.power, _sys.speed, _sys.acceleration);
 }
 
-bool hasCommand(char cmd)
+bool hasCommand(String cmd, char cmdId)
 {
     int index = 0;
     char c = 0;
-    while (c != cmd)
+    while (c != cmdId)
     {
-        if (index >= _sys.buffer.length())
+        if (index >= cmd.length())
         {
             return false;
         }
         else
         {
-            c = _sys.buffer.charAt(index);
+            c = cmd.charAt(index);
         }
         index++;
     }
     return true;
 }
-double getCommand(char cmd)
+double getCommand(String cmd, char cmdId)
 {
-    int index = _sys.buffer.indexOf(cmd);
-    int startIndex = _sys.buffer.indexOf(cmd) + 1;
+    int index = cmd.indexOf(cmdId);
+    int startIndex = cmd.indexOf(cmdId) + 1;
     char c = 0;
     while (c != ' ')
     {
         index++;
-        if (index >= _sys.buffer.length())
+        if (index >= cmd.length())
         {
             break;
         }
         else
         {
-            c = _sys.buffer.charAt(index);
+            c = cmd.charAt(index);
         }
     }
-    return _sys.buffer.substring(startIndex, index).toDouble();
+    return cmd.substring(startIndex, index).toDouble();
 }
